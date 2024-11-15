cbf742fd2eda20e5df6d31ace9499bb04c50b17b
Garvit Joshi <163440488+Garvit1000@users.noreply.github.com>
2024-11-15 14:57:51 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'https://flexhunt.onrender.com',
+  origin: process.env.CLIENT_URL || 'https://www.flexhunt.co',
   credentials: true
 }));
 app.use(express.json());
